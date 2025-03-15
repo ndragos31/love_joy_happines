6230f09e7284a5bb43e16022057216e8d669fb95
Norbert <bws@bwss-MacBook-Pro.local>
2025-03-15 12:54:37 +0200

    some navbar changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="pt-16">{children}</main>
       </body>
     </html>
   );
